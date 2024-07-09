aaacae115bd9f2d77ee273e13fe919bfee147403
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2024-07-09 13:12:27 +0300

    update Matches desgin

--- src/app/(Auth)/MyTournaments/[id]/Matches/Matches.jsx
+++ src/app/(Auth)/MyTournaments/[id]/Matches/Matches.jsx
@@ -1,12 +1,21 @@
+"use client";
+import { useState } from "react";
 import Head from "../HeadMyTournaments/HeadMyTournaments";
+import MatchesBox from "./MatchesBox/MatchesBox";
 import "./Matches.css";
 
 function Matches({ ShwonPage, SetShownPage }) {
+  const [LoadedMatches, SetLoadedMatches] = useState([{}, {}, {}, {}]);
   return (
     <div className="Matches">
       <Head Title={ShwonPage} SetShownPage={SetShownPage}>
         <button>Start next Stage</button>
       </Head>
+      <div className="Matchs-container">
+        {LoadedMatches.map((mat) => (
+          <MatchesBox />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/app/(Auth)/MyTournaments/[id]/Matches/MatchesBox/MatchesBox.jsx
@@ -0,0 +1,170 @@
+"use client";
+import Image from "next/image";
+import "./MatchesBox.css";
+import { useState } from "react";
+
+function MatchesBox() {
+  const [DetailsOpen, SetDetailsOpen] = useState(false);
+
+  return (
+    <div
+      className={
+        DetailsOpen ? "match-box-container" : "match-box-container open"
+      }
+    >
+      <div className="General-Info">
+        <h3>Group 2</h3>
+        <div className="actions">
+          <div className="time">
+            <span>12:14</span>
+            <Image src="/Matches/Time.svg" width={30} height={30} alt="Time" />
+          </div>
+          <div className="summary">
+            <Image
+              src="/Matches/Summary.svg"
+              width={30}
+              height={30}
+              alt="Summary"
+            />
+          </div>
+          <div className="Expand" onClick={() => SetDetailsOpen(!DetailsOpen)}>
+            <Image
+              src={
+                DetailsOpen
+                  ? "/Matches/arrow-down.svg"
+                  : "/Matches/arrow-up.svg"
+              }
+              width={30}
+              height={30}
+              alt="Summary"
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        className={
+          DetailsOpen ? "DetailsCotainer-box open" : "DetailsCotainer-box"
+        }
+      >
+        <div className="More-Matches-details">
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+          <div className="vs">
+            <Image src="/Matches/VS.svg" width={50} height={50} alt="VS" />
+          </div>
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+        </div>
+        <div className="More-Matches-details">
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+          <div className="vs">
+            <Image src="/Matches/VS.svg" width={50} height={50} alt="VS" />
+          </div>
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+        </div>
+        <div className="More-Matches-details">
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+          <div className="vs">
+            <Image src="/Matches/VS.svg" width={50} height={50} alt="VS" />
+          </div>
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+        </div>
+        <div className="More-Matches-details">
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+          <div className="vs">
+            <Image src="/Matches/VS.svg" width={50} height={50} alt="VS" />
+          </div>
+          <div className="match-box">
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+            <div className="user-box">
+              <span>T4</span>
+              <Image src="/User/User1.jpeg" width={30} height={30} alt="User" />
+              <h4>Ali Ahmad</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MatchesBox;
